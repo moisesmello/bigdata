1d106cbf0f85d0859332b5ed18803965f68d9f84
moisesmello <43097063+moisesmello@users.noreply.github.com>
2018-09-20 21:36:00 -0400

    Shell script exercise

---
+++ archiveFile.sh
@@ -0,0 +1,34 @@
+filename="mydatafile.dat"
+#**write a command to gzip the file
+
+today_dt=`date +%Y%m%d`
+
+#check if the directory already exists and capture the return code
+
+hadoop fs -ls /data/archive/$today_dt
+
+if [ $? != 0 ]
+then
+  #Directory doesn't exists
+  #**write command to create directory
+
+  hadoop fs -mkdir /data/archive/$today_dt
+
+else
+  #Directory exists. Now lets check if file exists. If so then delete it
+  hadoop fs -ls /data/archive/$today_dt/${filename}.gz
+
+  if [ $? == 0 ]
+  then
+
+   # **write a command to delete that file
+   hadoop fs -rm /data/archive/$today_dt/${filename}.gz
+
+  fi
+fi
+
+#Ok, so now we are all set with initial checks. Lets push the file from local to HDFS
+#**write a command to move file from Local file system to HDFS
+
+hadoop fs -moveFromLocal $filename /data/archive/$today_dt/${filename}.gz
+
