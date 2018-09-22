12248322c7b99fb6d8d5ffd5b9804a0d54ae871c
conted <conted@BH210-08.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2018-09-22 15:52:05 -0400

    Adding Hadoop Installation Guide and updatd MapReduce

--- FirstMapReduce/src/main/java/main/WebHitCounterMain.java
+++ FirstMapReduce/src/main/java/main/WebHitCounterMain.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -11,7 +12,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WebHitCounterMain {
     public static void main(String[] args) throws Exception {
         //System.setProperty("hadoop.home.dir", "D:\\hadoop-2.6.2\\");
+
         Configuration conf = new Configuration();
+
+        Path output = new Path(args[1]);
+        FileSystem hdfs = FileSystem.get(conf);
+
+        if (hdfs.exists(output)) {
+            hdfs.delete(output, true);
+        }
+
         Job job = Job.getInstance(conf, "Daily Web Hit Counter");
         job.setJarByClass(WebHitCounterMain.class);
         job.setMapperClass(mapper.WebHitCounterMapper.class);
